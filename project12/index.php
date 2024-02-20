7c0f71c49af31c9ccb0305b4497ebe472a0772c6
baodo <baodo@baodos-MacBook-Pro.local>
2024-02-20 20:11:44 +0700

    Add project12

---
+++ project12/index.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="assets/style.css">
+
+</head>
+
+<body>
+<?php
+$navItems = [
+    "GIỚI THIỆU",
+    "TIN TỨC & THÔNG BÁO",
+    "TUYỂN SINH",
+    "ĐÀO TẠO",
+    "NGHIÊN CỨU",
+    "ĐỐI NGOẠI",
+    "VĂN BẢN",
+    "SINH VIÊN",
+    "LIÊN HỆ"
+];
+?>
+
+<div class="nav-bar">
+    <div class="nav-bar-left">
+            <h2">Home Logo</h2>
+    </div>
+
+    <div class="nav-bar-right">
+        <ul>
+            <?php
+                foreach ($navItems as $item){
+                    echo "<li>$item</li>";
+                }
+            ?>
+        </ul>
+    </div>
+</div>
+</body>
+
+</html>
